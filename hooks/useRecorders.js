ae1a0a8ed3a8e2f0a3809e34d9afabbfa130c11c
Huy Khuong <scottkhuong@gmail.com>
2022-06-27 12:50:18 +0700

    changed audio file type

--- hooks/useRecorders.js
+++ hooks/useRecorders.js
@@ -119,7 +119,7 @@ export default function useRecorder(sendMessage) {
       }
 
       recorder.onstop = () => {
-        const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' })
+        const blob = new Blob(chunks, { type: 'audio/x-wav' })
         chunks = []
 
         setRecorderState((prevState) => {
